6627d0a55ee0c42eafa37e5b77df772f9852dc20
Anurag <idanurag567@gmail.com>
2023-04-18 01:20:47 +0530

    committed

--- HackAvenue 23'/New Folder 1/users_location/app.js
+++ HackAvenue 23'/New Folder 1/users_location/app.js
@@ -26,15 +26,48 @@
 //     center: coords,
 //     mapTypeId: google.maps.MapTypeId.ROADMAP,
 //   };
-//   var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-//   var marker = new google.maps.Marker({
-//     map: map,
-//     position: coords,
-//   });
+
 // }
 
-// function failure() {}
-
+// // function failure() {}(g) => {
+// var h,
+//   a,
+//   k,
+//   p = "The Google Maps JavaScript API",
+//   c = "google",
+//   l = "importLibrary",
+//   q = "__ib__",
+//   m = document,
+//   b = window;
+// b = b[c] || (b[c] = {});
+// var d = b.maps || (b.maps = {}),
+//   r = new Set(),
+//   e = new URLSearchParams(),
+//   u = () =>
+//     h ||
+//     (h = new Promise(async (f, n) => {
+//       await (a = m.createElement("script"));
+//       e.set("libraries", [...r] + "");
+//       for (k in g)
+//         e.set(
+//           k.replace(/[A-Z]/g, (t) => "_" + t[0].toLowerCase()),
+//           g[k]
+//         );
+//       e.set("callback", c + ".maps." + q);
+//       a.src = `https://maps.${c}apis.com/maps/api/js?` + e;
+//       d[q] = f;
+//       a.onerror = () => (h = n(Error(p + " could not load.")));
+//       a.nonce = m.querySelector("script[nonce]")?.nonce || "";
+//       m.head.append(a);
+//     }));
+// d[l]
+//   ? console.warn(p + " only loads once. Ignoring:", g)
+//   : (d[l] = (f, ...n) => r.add(f) && u().then(() => d[l](f, ...n)));
+// ({
+//   key: "AIzaSyA9bBt_iEQx1l2U0R73BRAorPoXoopMIXY",
+//   // Add other bootstrap parameters as needed, using camel case.
+//   // Use the 'v' parameter to indicate the version to load (alpha, beta, weekly, etc.)
+// });
 x = navigator.geolocation;
 
 x.getCurrentPosition(success, failure);
@@ -48,7 +81,7 @@ function success(position) {
   console.log(mylong);
   // google api ready latitude and longitude string
 
-  // var coords = new google.maps.LatLng(mylat, mylong);
+  var coords = new google.maps.LatLng(mylat, mylong);
   // var coords = new google.maps.LatLng(-34, 151);
   // setting up out google map
 
@@ -60,7 +93,12 @@ function success(position) {
   // mapTypeId: google.maps.MapTypeId.ROADMAP
   // CREATING MAP
 
+  // var map = new google.maps.Map(document.getElementById("map"), mapOptions);
   var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+  var marker = new google.maps.Marker({
+    map: map,
+    position: coords,
+  });
 }
 
 function failure() {
