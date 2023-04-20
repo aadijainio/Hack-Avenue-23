6c0d04288e6cc540b959330c2e811b4b682923dd
Anurag <idanurag567@gmail.com>
2023-04-20 12:52:03 +0530

    added redirection to maps

--- request/script.js
+++ request/script.js
@@ -13,6 +13,28 @@ const descriptions = [
   "Yatin goyal requested garbage pickup at location given on the map.",
 ];
 
+function getRandomLocation(latitude, longitude, radius) {
+  // Convert radius from meters to degrees
+  var radiusInDegrees = radius / 111000;
+
+  // Generate a random distance and angle
+  var u = Math.random();
+  var v = Math.random();
+  var w = radiusInDegrees * Math.sqrt(u);
+  var t = 2 * Math.PI * v;
+  var x = w * Math.cos(t);
+  var y = w * Math.sin(t);
+
+  // Adjust the x-coordinate for the shrinking of the east-west distances
+  var newLongitude = x / Math.cos(latitude);
+
+  // Calculate the new latitude and longitude
+  var newLatitude = latitude + y;
+  newLongitude = longitude + newLongitude;
+
+  return { lat: newLatitude, lng: newLongitude };
+}
+
 const numRequests = Math.floor(Math.random() * 10) + 1; // generate a random number of requests between 1 and 10
 
 const requestsContainer = document.getElementById("requests-container");
@@ -34,8 +56,45 @@ for (let i = 0; i < numRequests; i++) {
   const buttonElement = document.createElement("a");
   buttonElement.innerText = "maps";
   buttonElement.classList.add("map-btn");
-  buttonElement.href = "https://www.google.com/maps";
+  x = navigator.geolocation;
+  x.getCurrentPosition(success, failure);
 
+  let mylat;
+  var mylong;
+
+  function success(position) {
+    // fetch coordinates
+
+    var mylat = position.coords.latitude;
+    var mylong = position.coords.longitude;
+
+    // google api ready latitude and longitude string
+
+    var coords = new google.maps.LatLng(mylat, mylong);
+    // var coords = new google.maps.LatLng(-34, 151);
+    // setting up out google map
+    var myLocation = { lat: mylat, lng: mylong };
+    var randomLocation = getRandomLocation(
+      myLocation.lat,
+      myLocation.lng,
+      1000
+    );
+    console.log(mylat);
+    console.log(mylong);
+    mylat = mylat.toString();
+    mylong = mylong.toString();
+  }
+  function failure() {
+    $("#lat").html("<h3> No co-ordinates available!</h3>");
+  }
+
+  // console.log(x);
+
+  console.log(mylat);
+  console.log(mylong);
+  // buttonElement.href = `https://www.google.com/maps/dir/${mylat},${mylong}`;
+  // buttonElement.href = `https://www.google.com/maps/dir/28.6497737082171,77.13875073997447`;
+  buttonElement.href = `https://www.google.com/maps/dir/28.646288,77.141355`;
   requestElement.appendChild(titleElement);
   requestElement.appendChild(descriptionElement);
   requestElement.appendChild(buttonElement);
